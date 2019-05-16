5cd0e3eed5bf0b14fb607af59c0dca3673e8e801
aytaceminoglu <35230680+aytaceminoglu@users.noreply.github.com>
2019-05-16 07:32:42 +0300

    Update errors.blade.php

--- src/Views/bootstrap4/shared/errors.blade.php
+++ src/Views/bootstrap4/shared/errors.blade.php
@@ -1,5 +1,5 @@
 @if($errors->first() != '')
-    <div class="container">
+    <div class="container px-0">
         <div class="alert alert-danger">
             <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">{{ trans('ticketit::lang.flash-x') }}</span></button>
             <ul class="mb-0">
@@ -11,7 +11,7 @@
     </div>
 @endif
 @if(Session::has('warning'))
-    <div class="container">
+    <div class="container px-0">
         <div class="alert alert-danger">
             <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">{{ trans('ticketit::lang.flash-x') }}</span></button>
             {{ session('warning') }}
@@ -19,7 +19,7 @@
     </div>
 @endif
 @if(Session::has('status'))
-    <div class="container">
+    <div class="container px-0">
         <div class="alert alert-success">
             <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">{{ trans('ticketit::lang.flash-x') }}</span></button>
             {{ session('status') }}
