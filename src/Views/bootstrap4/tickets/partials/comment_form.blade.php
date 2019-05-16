291d5d80efb2ae04bf0464f90b3c388493460ccc
aytaceminoglu <35230680+aytaceminoglu@users.noreply.github.com>
2019-05-16 18:20:43 +0300

    Update comment_form.blade.php

--- src/Views/bootstrap4/tickets/partials/comment_form.blade.php
+++ src/Views/bootstrap4/tickets/partials/comment_form.blade.php
@@ -6,7 +6,7 @@
 
 {!! CollectiveForm::textarea('content', null, ['class' => 'form-control summernote-editor', 'rows' => "3"]) !!}
 
-{!! CollectiveForm::submit( trans('ticketit::lang.reply'), ['class' => 'btn btn-outline-primary pull-right mt-3 mb-3']) !!}
+{!! CollectiveForm::submit( trans('ticketit::lang.reply'), ['class' => 'btn btn-gradient-blue pull-right mt-3 mb-3']) !!}
 
 {!! CollectiveForm::close() !!}
     
